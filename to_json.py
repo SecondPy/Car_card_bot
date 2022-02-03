02633abcf8488869433592492d698dad2b164013
SecondPy <78914221+SecondPy@users.noreply.github.com>
2022-02-04 01:27:08 +0300

    Update to_json.py

--- to_json.py
+++ to_json.py
@@ -1,9 +1,1 @@
-import json
-ar = []
 
-with open('cenz.txt', 'r') as r:
-    for i in r:
-        n = i.lower().split(', ')
-        if n != '':
-            with open ('cenz.json', 'w', encoding='utf-8') as e:
-                json.dump(n, e)
