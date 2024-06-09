c34ee55e8a35bd76c905c0821ce0584499631eca
SecondPy <forb346@yandex.ru>
2024-06-09 17:49:33 +0300

    090624

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -37,24 +37,6 @@ class SendOrder(StatesGroup):
     phone = State()
 
 
-#async def main_menu_client_constructor(session: AsyncSession, tg_id: int) -> list:
-#    text = f'🛢 Адрес <b>ОйлЦентр</b>: Волжский, пл. Труда, 4а.\n📱 Тел: +78443210102 (9:00-18:00)\n'
-#    btns_data, sizes = dict(), list()
-#    btns_data['header withoud_data'] = '⬇️ Выберите действие ⬇️'
-#    sizes.append(1)
-#    btns_data['get_client_calendar'] = '🔧 Записаться 🔧'
-#    btns_data[f'get_client_history {tg_id}'] = '📜 История 📜'
-#    sizes.append(2)
-#    actual_orders = await client_orm.find_orders_with_tg_id(session=session, tg_id=tg_id, status='actual')
-#    if actual_orders:
-#        for order in actual_orders:
-#            text += f"\n-👨🏼‍🏭Ждём Вас на обслуживание {DateFormatter(order.begins).message_format}"
-#    else:
-#        text += f'\n-👨🏼‍🏭 записей о предстоящем обслуживании не найдено'
-#
-#    return [text, btns_data, sizes]
-
-
 @client_private_router.callback_query(StateFilter("*"), F.data.startswith('client_cancel'))
 async def cancel_handler(callback: types.CallbackQuery, state: FSMContext, bot: Bot, session: AsyncSession) -> None:
     bot.client_idle_timer[callback.message.from_user.id] = 0
@@ -153,7 +135,6 @@ async def get_contact(message: types.Message, bot: Bot, session: AsyncSession):
                 except: messege_is_deleted = True
     
     else: await message.delete()
-
 
 
 @client_private_router.callback_query(F.data.startswith('get_client_order'))
